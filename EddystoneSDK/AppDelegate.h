e10ceaad53742bd5cef10341bd55d36cdfeca966
Chengang <chengang@mokotechnology.com>
2018-08-08 16:41:30 +0800

    Initial Commit

---
+++ EddystoneSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EddystoneSDK
+//
+//  Created by aa on 2018/8/8.
+//  Copyright © 2018年 MK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
